65efcf603580a90271be2c01e9f02ffe0104d95b
toorcn <60379207+toorcn@users.noreply.github.com>
2023-07-08 03:33:07 +0800

    fixed events regenerating on refresh

---
+++ clearQRSession.php
@@ -0,0 +1,11 @@
+<?php
+   session_start();
+   unset($_SESSION['event_name']);
+   unset($_SESSION['endTime']);
+   unset($_SESSION['endTimeSQL']);
+   unset($_SESSION['event_organizerID']);
+   unset($_SESSION['event_code']);
+   
+   echo 'You have cleaned session';
+   header('Refresh: 1; URL = dashboard.php');
+?>
--- dashboard.php
+++ dashboard.php
@@ -11,30 +11,39 @@ if(isset($_SESSION['valid']) && $_SESSION['valid'] == true) {
     $participant = new participant($participantID);
 
     if($role == "Organizer") {
-        if(!(isset($_POST['event_name']) && isset($_POST['event_duration']))) {
-            ?>
-            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-                <!-- Event Name -->
-                <label for="event_name">Event Name: </label>
-                <input type="text" name="event_name" id="event_name" place required>
-                <br>
-                <!-- Event Duration -->
-                <label for="event_duration">Event Duration: </label>
-                <input type="radio" name="event_duration" id="event_duration" value="1" required>
-                <label for="1">1m</label>
-                <input type="radio" name="event_duration" id="event_duration" value="30" required>
-                <label for="30">30m</label>
-                <input type="radio" name="event_duration" id="event_duration" value="60" required>
-                <label for="60">60m</label>
-                <br>
-                <input type="submit" value="Submit">
-                <!-- Hidden -->
-                <input type="text" name="email" id="email" value="<?php echo $email ?>" hidden>
-                <input type="text" name="role" id="role" value="<?php echo $role ?>" hidden>                
-            </form>   
-            <?php            
-        } else {
-            // Event form submitted
+        // if (!(isset($_POST['event_name']) 
+        // && isset($_POST['event_duration']) 
+        // && isset($_SESSION['event_code']))) {
+        //     echo "true";
+        // } else echo "false";
+        // if (isset($_SESSION['event_code']))  {
+        //     echo "SESSION SET";
+        // } else echo "SESSION NOT SET";
+        if(!(isset($_POST['event_name']) && isset($_POST['event_duration'])) ) {
+            if (!isset($_SESSION['event_code'])) {
+                ?>
+                <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+                    <!-- Event Name -->
+                    <label for="event_name">Event Name: </label>
+                    <input type="text" name="event_name" id="event_name" place required>
+                    <br>
+                    <!-- Event Duration -->
+                    <label for="event_duration">Event Duration: </label>
+                    <input type="radio" name="event_duration" id="event_duration" value="1" required>
+                    <label for="1">1m</label>
+                    <input type="radio" name="event_duration" id="event_duration" value="30" required>
+                    <label for="30">30m</label>
+                    <input type="radio" name="event_duration" id="event_duration" value="60" required>
+                    <label for="60">60m</label>
+                    <br>
+                    <input type="submit" value="Submit">
+                    <!-- Hidden -->
+                    <input type="text" name="email" id="email" value="<?php echo $email ?>" hidden>
+                    <input type="text" name="role" id="role" value="<?php echo $role ?>" hidden>                
+                </form>   
+                <?php                 
+            }
+        } else { 
             $event_name = $_POST['event_name'];
             $event_duration = $_POST['event_duration'];
             $event_organizerID = getIdByEmail($email, $role);
@@ -46,84 +55,119 @@ if(isset($_SESSION['valid']) && $_SESSION['valid'] == true) {
             $endTimeSQL = $endTime->format('Y-m-d H:i:s');
             // echo "editted SQL time: " . $endTimeSQL . "<br>";
             // echo "<br>ID" . getIdByEmail($email, $role);
+            $_SESSION['event_name'] = $event_name;
+            $_SESSION['endTime'] = $endTime;
+            $_SESSION['endTimeSQL'] = $endTimeSQL;
+            $_SESSION['event_organizerID'] = $event_organizerID;
+            $_SESSION['event_code'] = $event_code;                
+            newEvent($event_organizerID, $event_name, $endTimeSQL, $event_code);
+        }
+            // if (!(isset($_SESSION['event_name']) && isset($_SESSION['endTime']) && isset($_SESSION['endTimeSQL']) && isset($_SESSION['event_organizerID']) && isset($_SESSION['event_code']))) {
+                // // Event form submitted
+                // $event_name = $_POST['event_name'];
+                // $event_duration = $_POST['event_duration'];
+                // $event_organizerID = getIdByEmail($email, $role);
+                // $event_code = (string)bin2hex(random_bytes(4));
+                // $event_code = strtoupper(substr($event_code, 0, 6));
 
-            if(newEvent($event_organizerID, $event_name, $endTimeSQL, $event_code)) {
-                $endTimeCountdown = $endTime->format('Y-m-d') . "T" . $endTime->format('H:i:s');
-                // echo "editted Countdown time: '" . $endTimeCountdown . "'<br>";
+                // $currentDateTime = new DateTime('now', new DateTimeZone('Asia/Singapore')); 
+                // $endTime = $currentDateTime->add(new DateInterval('PT' . $event_duration . 'M'));
+                // $endTimeSQL = $endTime->format('Y-m-d H:i:s');
+                // // echo "editted SQL time: " . $endTimeSQL . "<br>";
+                // // echo "<br>ID" . getIdByEmail($email, $role);
+                // $_SESSION['event_name'] = $event_name;
+                // $_SESSION['endTime'] = $endTime;
+                // $_SESSION['endTimeSQL'] = $endTimeSQL;
+                // $_SESSION['event_organizerID'] = $event_organizerID;
+                // $_SESSION['event_code'] = $event_code;                
+                // newEvent($event_organizerID, $event_name, $endTimeSQL, $event_code);
+        if (isset($_SESSION['event_name']) && isset($_SESSION['endTime']) 
+            && isset($_SESSION['endTimeSQL']) && isset($_SESSION['event_organizerID']) 
+            && isset($_SESSION['event_code'])) {
+            $event_name = $_SESSION['event_name'];
+            $endTime = $_SESSION['endTime'];
+            $endTimeSQL = $_SESSION['endTimeSQL'];
+            $event_organizerID = $_SESSION['event_organizerID'];
+            $event_code = $_SESSION['event_code'];
 
-                $qrIdentifier = $event_organizerID . ":" . $event_name . ":" . $event_code;
-                $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=$qrIdentifier";                
-                
-                $event_id = getEventIdByEventCode($event_code)["Event_ID"];
-                ?>
-                <div class="row">
-                    <div class="col">
-                        <h2><?php echo $event_name ?></h2>
-                        <img src='<?php echo $qrCodeUrl ?>' alt='QR Code'>
-                        <p>Event Code: <?php echo $event_code ?></p>
-                        (<span id="cntdwn"></span>)
-                        <script language="JavaScript">
-                            TargetDate = "<?php echo $endTimeCountdown ?>";
-                            CountActive = true;
-                            CountStepper = -1;
-                            LeadingZero = true;
-                            DisplayFormat = "%%M%% Minutes, %%S%% Seconds.";
-                        </script>
-                    </div>
-                    <div class="col">
-                        <div style="width:100%; height:100%;">
-                            <h4>Live Attendance (<span id="attendanceCount">0</span>)</h4>
-                            <div id="liveAttendance" style="border: 1px solid black; width:100%; height:100%;"></div>
-                        </div>
-                        <script language="JavaScript">
-                            function updateLiveAttendance() {
-                                // const thisPath = String(window.location.href).substr(7, 23);
-                                // console.log(thisPath + '/API/updateParticipantList.php')
-                                console.log('ULA');
-                                $.ajax({
-                                    type: "POST",
-                                    url: 'postGetEventArrays.php',
-                                    dataType: 'json',
-                                    data: {functionname: 'getParticipantByEventID', arguments: [<?php echo $event_id ?>]},
-                                    
-                                    success: (obj, textstatus) => {
-                                        // console.log({obj});
-                                        // const t = obj.result;
-                                        // console.log({t});
+        // if(newEvent($event_organizerID, $event_name, $endTimeSQL, $event_code)) {
 
-                                        // 
-                                        if( !('error' in obj) ) {
-                                            if (obj.result != null) {
-                                                let objResult = obj.result;
-                                                html = "";
+            $endTimeCountdown = $endTime->format('Y-m-d') . "T" . $endTime->format('H:i:s');
+            // echo "editted Countdown time: '" . $endTimeCountdown . "'<br>";
 
-                                                for(let i = 0; i < objResult.idArray.length; i++) {
-                                                    const participantId = objResult.idArray[i];
-                                                    const timestamp = objResult.timestampArray[i];
-                                                    const name = objResult.nameArray[i];
-                                                    const phone = objResult.phoneArray[i];
-                                                    html += "<p>(" + participantId + ") " + name + "[" + phone + "]- Scan time: " +  timestamp + "</p>";
-                                                } 
-                                                document.getElementById("liveAttendance").innerHTML = html;
-                                                document.getElementById("attendanceCount").innerHTML = objResult.idArray.length;
-                                            }
-                                        } else {
-                                            console.log(obj.error);
-                                        }
-                                    }
-                                });
-                                setTimeout(updateLiveAttendance, 1000);
-                            }
-                            updateLiveAttendance();
-                        </script>
-                    </div>
+            $qrIdentifier = $event_organizerID . ":" . $event_name . ":" . $event_code;
+            $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=$qrIdentifier";                
+            
+            $event_id = getEventIdByEventCode($event_code)["Event_ID"];
+            ?>
+            <div class="row">
+                <div class="col">
+                    <h2><?php echo $event_name ?></h2>
+                    <img src='<?php echo $qrCodeUrl ?>' alt='QR Code'>
+                    <p>Event Code: <?php echo $event_code ?></p>
+                    (<span id="cntdwn"></span>)
+                    <script language="JavaScript">
+                        TargetDate = "<?php echo $endTimeCountdown ?>";
+                        CountActive = true;
+                        CountStepper = -1;
+                        LeadingZero = true;
+                        DisplayFormat = "%%M%% Minutes, %%S%% Seconds.";
+                    </script>
                 </div>
+                <div class="col">
+                    <div style="width:100%; height:100%;">
+                        <h4>Live Attendance (<span id="attendanceCount">0</span>)</h4>
+                        <div id="liveAttendance" style="border: 1px solid black; width:100%; height:100%;"></div>
+                    </div>
+                    <script language="JavaScript">
+                        function updateLiveAttendance() {
+                            // const thisPath = String(window.location.href).substr(7, 23);
+                            // console.log(thisPath + '/API/updateParticipantList.php')
+                            console.log('ULA');
+                            $.ajax({
+                                type: "POST",
+                                url: 'postGetEventArrays.php',
+                                dataType: 'json',
+                                data: {functionname: 'getParticipantByEventID', arguments: [<?php echo $event_id ?>]},
+                                
+                                success: (obj, textstatus) => {
+                                    // console.log({obj});
+                                    // const t = obj.result;
+                                    // console.log({t});
 
-                <?php
-                
-            } else {
-                // newEvent Error
-            }
+                                    // 
+                                    if( !('error' in obj) ) {
+                                        if (obj.result != null) {
+                                            let objResult = obj.result;
+                                            html = "";
+
+                                            for(let i = 0; i < objResult.idArray.length; i++) {
+                                                const participantId = objResult.idArray[i];
+                                                const timestamp = objResult.timestampArray[i];
+                                                const name = objResult.nameArray[i];
+                                                const phone = objResult.phoneArray[i];
+                                                html += "<p>(" + participantId + ") " + name + "[" + phone + "]- Scan time: " +  timestamp + "</p>";
+                                            } 
+                                            document.getElementById("liveAttendance").innerHTML = html;
+                                            document.getElementById("attendanceCount").innerHTML = objResult.idArray.length;
+                                        }
+                                    } else {
+                                        console.log(obj.error);
+                                    }
+                                }
+                            });
+                            setTimeout(updateLiveAttendance, 1000);
+                        }
+                        updateLiveAttendance();
+                    </script>
+                </div>
+            </div>
+
+            <?php
+            
+        // } else {
+            // newEvent Error
+        // }
         }
     }
     if($role == "Participant") {
