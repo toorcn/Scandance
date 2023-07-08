271557de5fb93aeeb637993576d97d103fc5b695
toorcn <60379207+toorcn@users.noreply.github.com>
2023-07-08 20:42:28 +0800

    Added export to CSV functionality

---
+++ downloadCSV.php
@@ -0,0 +1,44 @@
+<?php
+
+$listForCSV = array();
+
+require('partials/database.php');
+
+// Create the Array
+$event_id = $_GET['eventID'];
+$event_name = $_GET['eventName'];
+
+$eventParticipants = getParticipantByEventID($event_id);
+$eventParticipants = json_decode($eventParticipants, true);
+foreach($eventParticipants as $participant) {
+    $participantID = array_values($participant)[0];
+    $participant = new participant($participantID);
+    $participantName = $participant->getName();
+    $participantPhone = $participant->getPhone();
+    $participantEmail = $participant->getEmail();
+    // for csv
+    $listForCSV[] = array($participantID, $participantName, $participantPhone, $participantEmail);
+    // 
+}
+// End of Create the Array
+
+// Open a file handle for writing using the fopen() function, and pass the file handle and the data array to the fputcsv() function.
+$filename = "export.csv";
+$filenameClient = "Scandance_" . $event_name . "_" . date("Y-m-d") . ".csv";
+$output = fopen($filename, "w");  
+fputcsv($output, array('Participant ID', 'Participant Name', 'Participant Phone', 'Participant Email')); 
+
+foreach($listForCSV as $row) {  
+    fputcsv($output, $row);  
+}  
+fclose($output);
+
+// Set the Content-Type and Content-Disposition headers to indicate that the file is a CSV file that should be downloaded.
+header("Content-Type: text/csv");
+header("Content-Disposition: attachment; filename=$filenameClient");
+
+// Output the contents of the file to the browser.
+readfile($filename);
+
+// header("Location: event.php?eventID=$event_id")
+?>
--- event.php
+++ event.php
@@ -3,8 +3,6 @@ require('partials/database.php');
 require('partials/header.php');
 
 if(isset($_GET['eventID'])) {
-    $email = $_SESSION['email'];
-    $role = $_SESSION['role'];
     $event_id = $_GET['eventID'];
     
     $event = getEventByEventID($event_id);
@@ -23,13 +21,22 @@ if(isset($_GET['eventID'])) {
             </div>
             <div class="col">
                 <h1>Event Participants</h1>
+
                 <?php
                 if ($eventParticipants == NULL) {
                     echo "<p>No participants joined this event.</p>";
                 } else {
+                    ?>
+                    <form action="downloadCSV.php" method="get">
+                        <input type="hidden" name="eventID" value="<?php echo $event_id ?>">
+                        <input type="hidden" name="eventName" value="<?php echo $event["Event_Name"] ?>">
+                        <input type="submit" name="export" value="Export to CSV" class="btn btn-primary" />
+                    </form>                    
+                    <?php
                     $eventParticipants = json_decode($eventParticipants, true);
+
                     foreach($eventParticipants as $participant) {
-                        $participantID = getIdByEmail($email, $role);
+                        $participantID = array_values($participant)[0];
                         $participant = new participant($participantID);
                         $participantName = $participant->getName();
                         $participantPhone = $participant->getPhone();
